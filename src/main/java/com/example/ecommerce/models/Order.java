6dcb9e762d39cdc2fed4aa068f8ead556eb8114f
nayanraj210401 <nayanraj.developer@gmail.com>
2024-12-22 13:07:21 +0530

    feat: added order management service

---
+++ src/main/java/com/example/ecommerce/models/Order.java
@@ -0,0 +1,47 @@
+package com.example.ecommerce.models;
+
+import com.example.ecommerce.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order extends BaseModel {
+
+    @Column(nullable = false, name = "total_amount")
+    private BigDecimal totalAmount;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+//    implemented later when auth is created
+//    @Column(name = "user_id")
+//    private Long userId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+        if(status == null) {
+            status = OrderStatus.PENDING;
+        }
+    }
+
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
