7781c7b877f63fa53f9f7e341292f5522485ae9c
nayanraj210401 <nayanraj.developer@gmail.com>
2025-01-18 17:55:44 +0530

    chore: fixed docker

---
+++ ecommerce-service/Move.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+
+public class Move {
+  public static void moveMavenRepo() {
+    String userHome = System.getProperty("user.home");
+    Path source = Path.of(userHome, ".m2");
+    Path target = Path.of(System.getProperty("user.dir"), "maven-dep");
+
+    if (!Files.exists(source)) {
+        System.err.println("Maven repository not found at: " + source);
+        return;
+    }
+
+    if (Files.exists(target)) {
+        System.out.println("Maven repository already exists at: " + target);
+
+        // delete it the folder
+        System.out.println("Deleting the existing Maven repository at: " + target);
+        try {
+            Files.walk(target)
+                .map(Path::toFile)
+                .sorted((o1, o2) -> -o1.compareTo(o2))
+                .forEach(File::delete);
+        } catch (IOException e) {
+            System.err.println("Failed to delete target directory: " + target);
+            e.printStackTrace();
+            return;
+        }
+
+    }else{
+        try {
+            Files.createDirectories(target);
+        } catch (IOException e) {
+            System.err.println("Failed to create target directory: " + target);
+            e.printStackTrace();
+            return;
+        }
+    }
+
+
+    try {
+        Files.walk(source)
+            .forEach(sourcePath -> {
+                Path targetPath = target.resolve(source.relativize(sourcePath));
+                try {
+                    Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+                } catch (IOException e) {
+                    System.err.println("Failed to copy: " + sourcePath + " to " + targetPath);
+                    e.printStackTrace();
+                }
+            });
+        System.out.println("Maven repository moved successfully.");
+    } catch (IOException e) {
+        System.err.println("Error during moving Maven repository.");
+        e.printStackTrace();
+    }
+}
+
+  public static void main(String[] args) {
+    moveMavenRepo();
+  }
+}
